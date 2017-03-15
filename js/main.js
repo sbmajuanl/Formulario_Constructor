fdc48c5599946eac6eec498cb4518ee58867b273
Brilly Majuan <sbmajuanl@gmail.com>
2017-03-14 23:18:27 -0500

    Subiendo cambios en js

--- js/main.js
+++ js/main.js
@@ -24,8 +24,24 @@ function enviardatos(nombre,apellido,edad,pais){
             "Pais :"+this.pais;
   }
    }
-    listado.push(new enviardatos(nombre,apellido,edad,pais));// pone los datos al final a traves del push
-    enviar.innerHTML= listado[listado.length-1].formulario();// Agrega los datos  y tiene -1 porque el ID comienza en uno
-    document.getElementById("formulario").reset() // Para que eliminen los datos del campo luego de enviar
+   listado.push(new enviardatos(nombre,apellido,edad,pais));// pone los datos al final a traves del push
+   enviar.innerHTML= listado[listado.length-1].formulario();// Agrega los datos  y tiene -1 porque el ID comienza en uno
+   document.getElementById("formulario").reset() // Para que eliminen los datos del campo luego de enviar
+
+    function printAll(arreglo){
+      var div = document.createElement('div');
+      div.setAttribute('class',"patient");
+      var p1 = document.createElement('p');
+      p1.innerHTML = "Nombre: " + arreglo.nombre + " " + arreglo.apellido
+      var p2 = document.createElement('p');
+      p2.innerHTML = "Edad: " + arreglo.edad
+      var p3 = document.createElement('p');
+      p3.innerHTML = "País: " + arreglo.pais
+      div.appendChild(p1);
+      div.appendChild(p2);
+      div.appendChild(p3);
+      return div;
+    }
+     enviar.appendChild(printAll(formulario));
       });
 });
